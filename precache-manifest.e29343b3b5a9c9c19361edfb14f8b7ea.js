52007e39987531de6138cc085a121c9ba92b422a
sangwook kim <mr.galien@gmail.com>
2020-10-10 14:59:02 +0900

    Updates

---
+++ precache-manifest.e29343b3b5a9c9c19361edfb14f8b7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e0725277fa485a663e3be403c4ea85",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "e100b683f60e9631adfa",
+    "url": "/nwitter/static/css/main.dda8c52d.chunk.css"
+  },
+  {
+    "revision": "410b16df614539b7bba6",
+    "url": "/nwitter/static/js/2.c2bfc596.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.c2bfc596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e100b683f60e9631adfa",
+    "url": "/nwitter/static/js/main.dfa175c2.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
